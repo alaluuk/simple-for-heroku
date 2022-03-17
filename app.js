4d659e811c9cf57f8099b2a78db32a1535692e30
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-03-17 15:39:38 +0200

    index.html valmis

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const { response } = require('express');
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express()
+dotenv.config()
 
 app.use(express.static('public'))
 
-var port;
-if(process.env.PORT != undefined){
-  port=process.env.PORT;
-}
-else {
-  port='3001';
-}
+var port=process.env.PORT;
+// if(process.env.PORT != undefined){
+//   port=process.env.PORT;
+// }
+// else {
+//   port='3001';
+// }
 
 app.listen(port, 
   function(){
